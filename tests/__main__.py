3abbec52dfb2e02d4bf7978e6fb9bfe607711e87
Ishan <contact@ishansharma.com>
2018-02-09 16:19:15 -0600

    Renaming test file to fix typo form ids to id3

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,17 +1,17 @@
 # code reference: https://docs.python.org/3.6/library/unittest.html#module-unittest
 import unittest
 
-from tests import test_ids_heuristics
+from tests import test_id3_heuristics
 
 
 def suite():
     test_suite = unittest.TestSuite()
 
     # tests for entropy function
-    test_suite.addTest(test_ids_heuristics.TestHeuristics('test_calculate_entropy'))
+    test_suite.addTest(test_id3_heuristics.TestHeuristics('test_calculate_entropy'))
 
     # tests for information gain
-    test_suite.addTest(test_ids_heuristics.TestInformationGainHeuristic('test_ig_heuristic'))
+    test_suite.addTest(test_id3_heuristics.TestInformationGainHeuristic('test_ig_heuristic'))
 
     return test_suite
 
